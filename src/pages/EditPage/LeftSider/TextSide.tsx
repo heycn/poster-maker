80698265da1f81a1c9a6664ef50baf3265fb9b73
heycn <heycn@foxmail.com>
2023-06-26 20:00:04 +0800

    可以拖拽添加组件了

--- src/pages/EditPage/LeftSider/TextSide.tsx
+++ src/pages/EditPage/LeftSider/TextSide.tsx
@@ -43,9 +43,16 @@ const TextSide = memo(() => {
       <ul className={leftSideStyles.box}>
         {settings.map((item) => (
           <li
+            draggable={true}
             key={item.value}
             className={leftSideStyles.item}
-            onClick={() => addCmp({ ...item, type: isTextComponent })}>
+            onClick={() => addCmp({ ...item, type: isTextComponent })}
+            onDragStart={(e) => {
+              e.dataTransfer.setData(
+                "drag-cmp",
+                JSON.stringify({ ...item, type: isTextComponent })
+              );
+            }}>
             {item.value.indexOf("双击编辑") > -1
               ? item.value.slice(4)
               : item.value}
--- src/store/editStoreTypes.ts
+++ src/store/editStoreTypes.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
-export type Style = React.CSSProperties;
+export type Style = any;
 
 export interface ICanvas {
   title: string;
