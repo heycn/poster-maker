8b6752be24bc835640bc87d00164bbaa5ee6e5ff
heycn <heycn@foxmail.com>
2023-07-11 19:46:50 +0800

    给组件加动画属性

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "antd/dist/reset.css";
 import "./index.css";
+import "./animation.css";
 
 // 如果在immer中使用Map/Set，则在你的应用程序入口文件
 import {enableMapSet} from "immer";
