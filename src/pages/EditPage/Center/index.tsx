0c0c52df2064b2ef4ce2b094aa2b0c8a85a2a8b4
heycn <heycn@foxmail.com>
2023-07-12 21:41:24 +0800

    组合组件的层级

--- src/pages/EditPage/Center/index.tsx
+++ src/pages/EditPage/Center/index.tsx
@@ -2,11 +2,13 @@ import styles from "./index.module.less";
 import Canvas from "./Canvas";
 import useEditStore, {
   addZIndex,
+  bottomZIndex,
   canvasStyleSelector,
   delSelectedCmps,
   setAllCmpsSelected,
   setCmpSelected,
   subZIndex,
+  topZIndex,
 } from "src/store/editStore";
 import Zoom from "./Zoom";
 import useZoomStore from "src/store/zoomStore";
@@ -41,13 +43,23 @@ export default function Center() {
         // 上移一层
         case "ArrowUp":
           e.preventDefault();
-          addZIndex();
+          if (e.shiftKey) {
+            // 置顶
+            topZIndex();
+          } else {
+            addZIndex();
+          }
           return;
 
         // 下移一层
         case "ArrowDown":
           e.preventDefault();
-          subZIndex();
+          if (e.shiftKey) {
+            // 置底部
+            bottomZIndex();
+          } else {
+            subZIndex();
+          }
           return;
       }
     }
