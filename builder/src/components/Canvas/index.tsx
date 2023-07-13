26672832f3882e4322ad7b40eebcdde2d7e72b15
heycn <heycn@foxmail.com>
2023-07-13 15:50:29 +0800

    生成器使用 SSG渲染

--- builder/src/components/Canvas/index.tsx
+++ builder/src/components/Canvas/index.tsx
@@ -1,4 +1,4 @@
-import Cmp, {isGroupComponent} from "src/components/Cmp";
+import Cmp, {ICmpWithKey, isGroupComponent} from "src/components/Cmp";
 import styles from "./index.module.css";
 
 export default function Canvas({canvas}: any) {
@@ -22,17 +22,17 @@ export default function Canvas({canvas}: any) {
       style={{
         ...style,
         width,
-        backgroundImage: `url(${(style as any).backgroundImage})`,
         transform,
         transformOrigin: "0 0",
         overflow: "hidden",
         margin: "auto",
+        backgroundImage: `url(${(style as any).backgroundImage})`,
       }}>
-      {cmps.map((item: any, index: number) =>
-        (item.type & isGroupComponent) === 0 ? (
-          <Cmp key={item.key} cmp={item} index={index} />
-        ) : null
-      )}
+      {cmps.map((cmp: ICmpWithKey, index: number) => {
+        return (cmp.type & isGroupComponent) === 0 ? (
+          <Cmp key={cmp.key} cmp={cmp} index={index} />
+        ) : null;
+      })}
     </div>
   );
 }
